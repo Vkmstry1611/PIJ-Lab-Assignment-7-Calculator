baad8de4f4dfaf926a44b1e1f31b368a1b4f5190
unknown <vidish162005@gmail.com>
2025-04-19 00:29:10 +0530

    added root and cube  function

--- Calculator.java
+++ Calculator.java
@@ -21,4 +21,22 @@ public class Calculator {
         }
         return (double) numbers[0] / numbers[1];
     }
+
+    // Square of a number
+    public int square(int number) {
+        return number * number;
+    }
+
+    // Cube of a number
+    public int cube(int number) {
+        return number * number * number;
+    }
+
+    // Square root of a number with exception handling for negative numbers
+    public double squareRoot(int number) throws IllegalArgumentException {
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot compute the square root of a negative number.");
+        }
+        return Math.sqrt(number);
+    }
 }
